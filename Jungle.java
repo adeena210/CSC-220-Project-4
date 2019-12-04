6e2f991637fa76943c4024599eb80c1efcb09c72
lalimabhola <57497021+lalimabhola@users.noreply.github.com>
2019-12-04 16:46:42 -0500

    Update Jungle.java

--- Jungle.java
+++ Jungle.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 
 public class Jungle{
 	
-	HashMap<String, float> properties= new HashMap<String, float>();
+	HashMap<String, Float> properties= new HashMap<>();
 	private int capacity;
 	private String status;
 	private ArrayList<Life> lives;
